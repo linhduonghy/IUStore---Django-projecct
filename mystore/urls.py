bba280ed81d7f9a6de2729f101fc702048dd4bc0
linhduonghy <linhduonghy@gmail.com>
2021-05-28 08:52:34 +0700

    add cart.html

--- mystore/urls.py
+++ mystore/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .view import index
+from .view import index, cart
 
 app_name = 'mystore'
 
 urlpatterns = [
     path('', index.home, name='home'),
+    path('cart', cart.cart, name = 'cart'),
 ]
